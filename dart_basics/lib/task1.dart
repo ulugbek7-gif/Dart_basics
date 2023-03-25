c8012d4d8aec4e822f6f9cda962279f36eb2f00b
ulugbek7-gif <128095332+ulugbek7-gif@users.noreply.github.com>
2023-03-25 18:16:02 +0500

    Add files via upload

--- dart_basics/lib/task1.dart
+++ dart_basics/lib/task1.dart
@@ -1,3 +1,13 @@
-class Man{
-  
+class DelimetersCalculator {
+  int greatestCommonDivisor(int a, int b) {
+
+    while (a != b) {
+      if (a > b) {
+        a = a - b;
+      } else
+        b = b - a;
+    }
+
+    return a;
+  }
 }
