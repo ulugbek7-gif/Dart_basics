514407cf9aad83593c03b588298124b5e43622eb
ulugbek7-gif <128095332+ulugbek7-gif@users.noreply.github.com>
2023-03-27 13:17:18 +0500

    Add files via upload

--- dart_basics/bin/dart_basics.dart
+++ dart_basics/bin/dart_basics.dart
@@ -1,9 +1,14 @@
 import 'package:dart_basics/task1.dart';
+import 'package:dart_basics/task1part2.dart';
 
 void main() {
   var gcd = DelimetersCalculator();
-  int a = 24;
-  int b = 54;
+  int a = 50;
+  int b = 70;
   print(gcd.greatestCommonDivisor(a, b));
-print(gcd.lcm(a, b));
+  print(gcd.lcm(a, b));
+
+  var spisok = Mulptiplier();
+  int x = 24;
+  print(spisok.numbers(x));
 }
