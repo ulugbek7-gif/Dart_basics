a5aec9734ed28ac6930d6559a92516aad01eef6c
ulugbek7-gif <128095332+ulugbek7-gif@users.noreply.github.com>
2023-03-28 23:35:20 +0500

    Add files via upload

--- dart_basics/lib/task2.dart
+++ dart_basics/lib/task2.dart
@@ -1,6 +1,12 @@
 class DecimalNumber {
-  String dcm(int a) {
-    String binaryNumber = a.toRadixString(2);
+  String dcm(int g) {
+    String binaryNumber = g.toRadixString(2);
     return binaryNumber;
   }
+  int mcd (int g){
+    String binaryNumber = g.toRadixString(2);
+    int bmc = int.parse(binaryNumber, radix: 2);
+    return bmc;
+  }
 }
+
