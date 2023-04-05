a9dc1127c8383716edc88828404ee58bd44cf8f0
ulugbek7-gif <128095332+ulugbek7-gif@users.noreply.github.com>
2023-04-06 00:20:17 +0500

    task № 3

--- dart_basics/lib/task3.dart
+++ dart_basics/lib/task3.dart
@@ -1,6 +1,6 @@
 class PrintNumbers {
-  methods (List text) {
-    List numbers = text.where((element) => element is int).toList();
+  List printnumber (List text) {
+    List numbers = text.whereType<int>().toList();
     return numbers;
   }
 }
