9286bb91857c4612126e733c8994b32fa1373cb6
ulugbek7-gif <128095332+ulugbek7-gif@users.noreply.github.com>
2023-04-06 00:21:56 +0500

    corrected the mistakes

--- dart_basics/bin/dart_basics.dart
+++ dart_basics/bin/dart_basics.dart
@@ -19,7 +19,7 @@ void main() {
   print(finding.dcm(g));
   print(finding.mcd(g));
 
-  var findnumbers = PrintNumbers();
+  var method = PrintNumbers();
   List<dynamic> text = [22, "миллион", 556, "магазин", 5];
-  print(findnumbers.methods(text));
+  print(method.printnumber(text));
 }
