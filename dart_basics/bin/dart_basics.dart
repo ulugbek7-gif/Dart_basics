95b051609b89b7c41fa032b90c683993d66efeeb
ulugbek7-gif <128095332+ulugbek7-gif@users.noreply.github.com>
2023-04-07 00:19:55 +0500

    task № 2 corrected

--- dart_basics/bin/dart_basics.dart
+++ dart_basics/bin/dart_basics.dart
@@ -2,6 +2,7 @@ import 'package:dart_basics/task1.dart';
 import 'package:dart_basics/task1part2.dart';
 import 'package:dart_basics/task2.dart';
 import 'package:dart_basics/task3.dart';
+import 'package:dart_basics/task4.dart';
 
 void main() {
   var gcd = DelimetersCalculator();
@@ -16,8 +17,10 @@ void main() {
 
   var finding = DecimalNumber();
   int g = 10;
-  print(finding.dcm(g));
-  print(finding.mcd(g));
+  String binaryG = finding.dcm(g);
+  int bmcG = finding.mcd(binaryG);
+  print(binaryG);
+  print(bmcG);
 
   var method = PrintNumbers();
   List<dynamic> text = [22, "миллион", 556, "магазин", 5];
