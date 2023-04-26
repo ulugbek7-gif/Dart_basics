a7d8bc4d58ea39b90c8ecb6f467b1be1641d576a
ulugbek7-gif <128095332+ulugbek7-gif@users.noreply.github.com>
2023-04-26 19:04:51 +0500

    task 7

---
+++ dart_basics/lib/task7.dart
@@ -0,0 +1,33 @@
+extension Root on num {
+  double pow(int n) {
+    double result = 1;
+    for (int i = 0; i < n; i++) {
+      result *= this;
+    }
+    return result;
+  }
+
+  double nthRoot(int n) {
+    if (n <= 0) {
+      throw ArgumentError("The exponent n must be greater than zero.");
+    }
+
+    if (this < 0 && n % 2 == 0) {
+      throw Exception("Cannot calculate even root of negative number.");
+    }
+
+    if (this == 0) {
+      return 0;
+    }
+
+    double x = this / n;
+    double nx = (n - 1) * x / n + this / (n * pow(n - 1));
+
+    while ((nx - x).abs() > 0.000001) {
+      x = nx;
+      nx = (n - 1) * x / n + this / (n * pow(n - 1));
+    }
+
+    return nx;
+  }
+}
